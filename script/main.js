133dbf971867108e4674fe92eb741c170d8568e9
Denniswachera <denniswachera@yahoo.com>
2023-07-31 09:45:15 +0300

    Add audio file

---
+++ script/main.js
@@ -0,0 +1,9 @@
+const burgerIcon = document.querySelector('.header__burger');
+const menu = document.querySelector('.menu');
+burgerIcon.addEventListener('click', (e) => {
+	if (e.target.closest('.header__burger')) {
+		burgerIcon.classList.toggle('_active')
+		document.body.classList.toggle('_lock');
+		menu.classList.toggle('_active');
+	}
+})
